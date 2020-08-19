d063f2f2b174592f12f321e8b3bce1987715968e
TA Thanh Dinh <tathanhdinh@gmail.com>
2020-08-19 17:25:56 +0200

    start async branch

--- src/pdb.rs
+++ src/pdb.rs
@@ -1,225 +1,110 @@
 use std::{
-    convert, fmt, fs,
-    io::{BufWriter, Read, Write},
-    ops::{Generator, GeneratorState},
-    path::{Path, PathBuf},
-    pin::Pin,
-    marker::Unpin,
+    mem::{size_of, size_of_val},
+    path::Path,
 };
 
-use {
-    bytes::BytesMut,
-    goblin::pe,
-    hex::encode_upper,
-    reqwest::{header::USER_AGENT, Client, Url},
-    uuid::Uuid,
-};
+use anyhow::{anyhow, bail, Result};
 
-use crate::error::{Error, OtherErrors, Result};
+use async_fs::File;
+use futures_lite::io::{AsyncReadExt, AsyncSeekExt, SeekFrom};
 
-// thread_local! {
-//     static PDBGET_USER_AGENT: UserAgent = UserAgent::new("Microsoft-Symbol-Server/10.0.0.0");
-// }
-static PDBGET_USER_AGENT: &str = "Microsoft-Symbol-Server/10.0.0.0";
-const DOWNLOAD_DATA_BUFFER_SIZE: usize = 4 * 1024;
-
-pub(super) struct Pdb {
-    name: String,
-    guid: Uuid,
-    age: u32,
+macro_rules! read_from_file {
+    ($f:expr, $t:ty) => {{
+        let mut buffer = [0u8; size_of::<$t>()];
+        $f.read_exact(&mut buffer).await?;
+        <$t>::from_le_bytes(buffer)
+    }};
 }
 
-impl Pdb {
-    pub fn from<P: AsRef<Path> + fmt::Debug>(file: P) -> Result<Self> {
-        let mut buffer = vec![];
-        let pe_obj = {
-            let mut fd = fs::File::open(&file)?;
-            fd.read_to_end(&mut buffer)?;
-            pe::PE::parse(&buffer)?
-        };
+pub async fn pdb_path(file: impl AsRef<Path>) -> Result<String> {
+    const IMAGE_FILE_HEADER_SIZE: usize = 0x14;
+    const IMAGE_DATA_DIRECTORY_OFFSET32: usize = 0x60;
+    const IMAGE_DATA_DIRECTORY_OFFSET64: usize = 0x70;
+    const IMAGE_DATA_DIRECTORY_SIZE: usize = 0x8;
+    const IMAGE_DIRECTORY_ENTRY_DEBUG: usize = 06;
+    const IMAGE_NUMBEROF_DIRECTORY_ENTRIES: usize = 0x10;
 
-        let debug_data = pe_obj
-            .debug_data
-            .ok_or_else(|| OtherErrors::PeDebugNotFound(format!("{:?}", file)))?;
-        let codeview_pdb70 = debug_data
-            .codeview_pdb70_debug_info
-            .ok_or_else(|| OtherErrors::PeCodeViewPdbNotFound(format!("{:?}", file)))?;
+    let mut file = File::open(file).await?;
 
-        let name = {
-            let base_name = codeview_pdb70
-                .filename
-                .split(|&c| c == b'\\')
-                .last()
-                .unwrap_or(codeview_pdb70.filename);
-
-            let name = String::from_utf8(base_name.to_vec())
-                .map_err(|_| OtherErrors::PdbBadName(format!("{:?}", file)))?;
-
-            let name = name.trim_matches(char::from(0));
-            name.to_owned()
-        };
-
-        let guid = Uuid::from_bytes(codeview_pdb70.signature);
-
-        Ok(Self {
-            name,
-            guid,
-            age: codeview_pdb70.age,
-        })
+    // check MZ header
+    let mz = read_from_file!(file, u16);
+    if mz != 0x4d5a {
+        bail!("bad MZ header");
     }
 
-    // pub fn download<P: AsRef<Path>, U: AsRef<Url>>(&self, symbol_server_url: &Url, dir: P) -> Result<PathBuf> {
-    pub fn download<P: AsRef<Path>, U: AsRef<str>>(
-        &self,
-        symbol_server_url: U,
-        dir: P,
-    ) -> Result<PathBuf>
-    where
-        PathBuf: convert::From<P>,
-    {
-        // Microsoft GUID encoding
-        let pdb_guid_age = {
-            let (b0, b1, b2, b3) = self.guid.as_fields();
-            format!(
-                "{:08X}{:04X}{:04X}{}{:X}",
-                b0.swap_bytes(),
-                b1.swap_bytes(),
-                b2.swap_bytes(),
-                encode_upper(b3),
-                self.age
-            )
-        };
+    // read e_lfanew
+    file.seek(SeekFrom::Current(0x3a)).await?;
+    let e_lfanew = read_from_file!(file, u32);
 
-        // send GET request and get response
-        let mut response = {
-            let url = {
-                let new_url_path = format!(
-                    "{}/{}/{}/{}",
-                    symbol_server_url.as_ref(),
-                    &self.name,
-                    &pdb_guid_age,
-                    &self.name
-                );
-                Url::parse(&new_url_path)?
-            };
-
-            let client = Client::new();
-            client
-                .get(url)
-                .header(USER_AGENT, PDBGET_USER_AGENT)
-                .send()?
-
-            // PDBGET_USER_AGENT
-            //     .with(|agent| {
-            //         let client = Client::new();
-            //         client.get(url.clone()).header(agent.clone()).send()
-            //     })
-            //     .map_err(Error::Connection)?
-        };
-
-        // check response
-        let status = response.status();
-        if !status.is_success() {
-            // fail_with_application_error!(format!(
-            //     "bad response, url: {}, code: {}",
-            //     response.url(),
-            //     status
-            // ));
-            return Err(OtherErrors::ServerBadResponse(format!(
-                "url: {}, code: {}",
-                response.url(),
-                status
-            ))
-            .into());
-        }
-
-        // prepare file for data
-        let file_path = {
-            let mut file_path = PathBuf::from(dir);
-            file_path.push(&self.name);
-            file_path.push(&pdb_guid_age);
-            file_path.push(&self.name);
-            file_path
-        };
-
-        let mut file = {
-            let file_dir = file_path.parent().unwrap();
-            fs::create_dir_all(file_dir)?;
-            let file = fs::File::create(&file_path)?;
-            BufWriter::new(file)
-        };
-
-        // download data and save
-        let mut data_buffer = BytesMut::with_capacity(DOWNLOAD_DATA_BUFFER_SIZE);
-        unsafe { data_buffer.set_len(data_buffer.capacity()) };
-        loop {
-            let count = response.read(&mut data_buffer)?;
-            if count == 0 {
-                break;
-            }
-            file.write_all(&data_buffer[0..count])?;
-        }
-        // for byte in response.bytes() {
-        //     // let byte = byte?;
-        //     let count = file.write(&[byte?])?;
-        //     if count == 0 {
-        //         fail_with_application_error!(format!("cannot write data, file: {:?}", file_path));
-        //     }
-        // }
-
-        Ok(file_path)
-    }
-}
-
-#[allow(dead_code)]
-type YT = Pdb;
-type RT = Result<()>;
-
-pub(super) struct PdbGenerator {
-    pe_files: Vec<PathBuf>,
-}
-
-impl PdbGenerator {
-    pub(super) fn new(pe_files: Vec<PathBuf>) -> Self {
-        PdbGenerator { pe_files }
+    // seek to the image NT header
+    file.seek(SeekFrom::Start(e_lfanew as _)).await?;
+    let pe_signature = read_from_file!(file, u32);
+    if pe_signature != 0x5045  {
+        bail!("bad image file header");
     }
 
-    pub(super) fn into_iter(self) -> impl Iterator<Item = YT> {
-        fn pdb_generator(pe_files: Vec<PathBuf>) -> impl Generator<Yield = YT, Return = RT> {
-            move || {
-                for pe in pe_files {
-                    if let Ok(pdb) = Pdb::from(&pe) {
-                        yield pdb
-                    } else {
-                        log::warn!("not a PE: {:#?}", &pe);
-                    }
-                }
-
-                Err(Error::StopGeneration)
-            }
-        }
-
-        fn gen_to_iter<G: Unpin>(g: G) -> impl Iterator<Item = G::Yield>
-        where
-            G: Generator<Return = RT>,
-        {
-            struct It<G>(G);
-
-            impl<G: Unpin + Generator<Return = Result<()>>> Iterator for It<G> {
-                type Item = G::Yield;
-
-                fn next(&mut self) -> Option<Self::Item> {
-                    // match unsafe { self.0.resume() } {
-                    match Pin::new(&mut self.0).as_mut().resume() {
-                        GeneratorState::Yielded(y) => Some(y),
-                        GeneratorState::Complete(_) => None,
-                    }
-                }
-            }
-
-            It(g)
-        }
-
-        gen_to_iter(pdb_generator(self.pe_files))
+    // now at the image file header
+    let image_file_machine = read_from_file!(file, u16);
+    if image_file_machine != 0x014c && image_file_machine != 0x8664 {
+        bail!("bad image file machine");
     }
+
+    // use it later
+    let number_of_sections = read_from_file!(file, u16);
+
+    // seek to the image optional header (since it comes immediately after the image file header)
+    file.seek(SeekFrom::Current(
+        (IMAGE_FILE_HEADER_SIZE - size_of_val(&pe_signature)) as i64,
+    ))
+    .await?;
+    let optional_header_magic = read_from_file!(file, u16);
+    if optional_header_magic != 0x010b && optional_header_magic != 0x020b {
+        bail!("bad image optional header magic");
+    }
+
+    let is_pe64 = if image_file_machine == 0x014c && optional_header_magic == 0x010b {
+        false
+    } else if image_file_machine == 0x8664 && optional_header_magic == 0x020b {
+        true
+    } else {
+        bail!("confict between file machine and optional header magic");
+    };
+
+    // seek to the image data directory.. then seek to the debug entry
+    file.seek(SeekFrom::Current(
+        (if is_pe64 {
+            IMAGE_DATA_DIRECTORY_OFFSET64
+        } else {
+            IMAGE_DATA_DIRECTORY_OFFSET32
+        } - size_of_val(&optional_header_magic)
+            + IMAGE_DIRECTORY_ENTRY_DEBUG * IMAGE_DATA_DIRECTORY_SIZE) as _,
+    ))
+    .await?;
+    let debug_directory_rva = read_from_file!(file, u32);
+
+    // seek to the image section hearder
+    file.seek(SeekFrom::Current(
+        ((IMAGE_NUMBEROF_DIRECTORY_ENTRIES - IMAGE_DIRECTORY_ENTRY_DEBUG)
+            * IMAGE_DATA_DIRECTORY_SIZE
+            - size_of_val(&debug_directory_rva)) as i64,
+    ))
+    .await?;
+    // try to map sections, virtually
+    for _ in 0..number_of_sections {
+        file.seek(SeekFrom::Current(
+            (size_of::<u8>() + size_of::<u32>()) as i64,
+        ))
+        .await?;
+        let rva = read_from_file!(file, u32);
+        let size_of_raw_data = read_from_file!(file, u32);
+
+        if rva <= debug_directory_rva && debug_directory_rva < rva + size_of_raw_data {
+            let pointer_to_raw_data = read_from_file!(file, u32);
+
+            // seek to the image debug directory
+            file.seek(SeekFrom::Start(pointer_to_raw_data as _)).await?;
+        }
+    }
+
+    todo!()
 }
